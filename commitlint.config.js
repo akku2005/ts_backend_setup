f79bb0586965f3588840367cc4b5652f61f1f035
Akash Kumar Sahu <130211152+akku2005@users.noreply.github.com>
2024-08-23 17:46:51 +0530

    feat(docker): Add Docker setup

--- commitlint.config.js
+++ commitlint.config.js
@@ -25,3 +25,11 @@ module.exports = {
     ],
   },
 };
+
+// module.exports = {
+//   extends: ['@commitlint/config-conventional'],
+//   rules: {
+//     'subject-empty': [2, 'never'],
+//     'type-empty': [2, 'never'],
+//   },
+// };
